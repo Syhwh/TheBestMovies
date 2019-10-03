81a9ca4ccdbf51fa20ce76c88dc33cee74bd858d
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-02 19:29:58 -0500

    Add trailer and card

--- js/appPromises.js
+++ js/appPromises.js
@@ -4,23 +4,82 @@ const fetchMovies = (page) => {
     return movies
 }
 
-const displayMovie = (data) => {
-    const source = document.getElementById('movies').innerHTML;
+const fetchVideo = (id)=>{
+    return fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=3d7fd0461ae8d0f2e808c37fb41950d7&language=en-US`)
+     .then(response=>response.json())
+}
+
+const fetchGenre = ()=>{
+   
+    return fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=3d7fd0461ae8d0f2e808c37fb41950d7&language=en-US")
+        .then((response) => response.json())
+      
+}
+
+const getGenre =()=>{
+    let data;
+  
+  console.log('data en get'+data)
+    return data
+}
+
+// const displayGenre = (id)=>{
+//     console.log({id})
+//     const source = document.getElementById('genre').innerHTML;
+//     const template = Handlebars.compile(source);
+//     const generatedHtml = template(id);
+//     const show = document.getElementById('genres');    
+//     show.innerHTML = generatedHtml;
+// }
+
+const displayMovie = (data,genre) => {
+
+    // data.results.forEach(element => {
+    //     element.genre_ids.forEach(ele=>{
+    //         Object.keys(ele).forEach(function(key) {
+    //             console.log(key)
+    //     })
+    //       })
+    // });
+
+    
+
+    const source = $("#movies").html();
     const template = Handlebars.compile(source);
-    const generatedHtml = template(data);
-    const show = document.getElementById('movies-container');
-    console.log('Esto es show' + generatedHtml)
-    show.innerHTML = generatedHtml;
+    const context={data, genre}
+    const generatedHtml = template(context);
+    $("#movies-container").html(generatedHtml)   
+    
+    console.log({data})
+     console.log({genre})
+    // const sourceG = document.getElementById('genre').innerHTML;
+    // const templateG = Handlebars.compile(sourceG);
+    // const generatedHtmlG = templateG(genre);
+    // const showG = document.getElementById('genres');    
+    // showG.innerHTML = generatedHtmlG;
+    
+
+    // displayGenre(data)
 }
 
-const importMovies = () => {
-    $("#import").hide();
-    fetchMovies(1).then((e => {
-        displayMovie(e);
+const importMovies = () => {    
+    fetchMovies(1)
+    .then((movies=> {
+        fetchGenre()
+        .then((genres=>{
+            displayMovie(movies,genres);
+            //displayGenre (genres)
+         }))
+      
+        
     }))
+    
+    
+
 }
 
-$("#import").on("click", importMovies);
+importMovies()
+//$("#import").on("click", importMovies);
 
 $('.page').on('click', function () {
     event.preventDefault();
@@ -31,3 +90,20 @@ $('.page').on('click', function () {
 
 })
 
+$('#movies-container').on('click','.trailer', function (e) {
+   // 
+   const id= parseInt($(e.currentTarget).attr('id'))
+   fetchVideo(id)
+   .then(e=>{
+    
+    $('.videoPlayer').html(`<iframe width="790" height="451" 
+    src="https://www.youtube.com/embed/${e.results[0].key}" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>`)
+    $('#trailerModal').modal('show');
+    
+   })
+   
+   
+    
+})
